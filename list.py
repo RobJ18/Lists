6aa06a8e82a8a639b145d79ff04702e4e40f6a46
RobJ18 <63207627+RobJ18@users.noreply.github.com>
2022-10-30 00:07:24 +0100

    Create list.py

---
+++ list.py
@@ -0,0 +1,25 @@
+import time
+
+thislist = []
+
+thislist.append(input('What is the first item you would like to add to your list?\n'))
+
+time.sleep(.5)
+
+while thislist:
+    listedit = input('\nWould like to:\n1. Add another item\n2. Print curent items in the list\n3. Exit\n')
+    time.sleep(.5)
+
+    if listedit == '1':
+        thislist.append(input('\nWhat would you like to add? '))
+        time.sleep(.5)
+
+    if listedit == '2':
+        print('\nCurrently these items are in your list: ', thislist)
+        time.sleep(.5)
+    
+    if listedit ==  '3':
+        print('Exiting list!')
+        break
+    
+exit
